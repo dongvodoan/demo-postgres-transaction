ecdb239e7b0907a17ee7507fffb25c2d73e6ec28
tienlv <tienlv@bap.jp>
2017-10-12 17:26:50 +0700

    update mongo url

--- config/configuration.js
+++ config/configuration.js
@@ -3,7 +3,7 @@
 module.exports = {
     port: 8000,
     secret: 'kbrserver',
-    db_url: 'mongodb://bap:123456@ds149844.mlab.com:49844/kbr',
+    db_url: 'mongodb://gameserver_kuberacoin:BIxeMEL5miVQ@localhost/gameserver_kuberacoin',
     FAUCET_AMOUNT: 5000, //  token
     FAUCET_TIME: 60 ,//  minute
     FAUCET_BALANCE: 1000, //  to faucet: balance <1000
