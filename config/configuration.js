dd510f0fabb84f55e0b40276bb2cb90d713735c3
Võ Doãn Đồng <dongvd@bap.jp>
2017-12-13 14:28:53 +0800

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -3,7 +3,7 @@
 module.exports = {
     port: 8002,
     secret: 'kbrserver',
-    db_url: 'mongodb://localhost:27017/gameserver_kuberacoin',
+    db_url: 'mongodb://KBRSERVERSTAGING:SOh3TbYhx8ypJPxmt1oOfL@localhost/KBRSERVERSTAGING',
     // db_url: 'mongodb://bap:123456@ds149844.mlab.com:49844/kbr',
     FAUCET_AMOUNT: 5000, //  token
     FAUCET_TIME: 60 ,//  minute
