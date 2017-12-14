6957159f360028ad6838d6fe01d69039eb240aaf
dongvd <dongvd@bap.jp>
2017-12-14 15:14:47 +0700

    fix cron task

--- api/services/cronService.js
+++ api/services/cronService.js
@@ -11,19 +11,20 @@ module.exports = {
                 }).populate('gameType', 'appId');
                 console.log(`${transactions.length} transactions not completed!`);
                 for (let i = 0; i < transactions.length; i++) {
-                    let transaction = transactions[i];
+                    let transaction = transactions[i];                   
                     let transactionId = transaction._id;
                     let user = transaction.user;
                     let amount = transaction.amount;
                     let method = transaction.method;
                     let appId = transaction.gameType.appId;
-                    if (transaction.mothod === historyTransaction.methods.subtractCoin) {
-                        let resDataSub = transactionRepository.postSubtractAmount(user, amount, appId);
+                    if (parseInt(method) === historyTransaction.methods.subtractCoin) {
+                        let resDataSub = await transactionRepository.postSubtractAmount(user, amount, appId);
+                        console.log(resDataSub);
                         if (resDataSub.status === 200) {
                             await historyTransaction.findByIdAndUpdate({ _id: transactionId}, { $set: { status: 1 }});
                         }
-                    } else if (transaction.mothod === historyTransaction.methods.addCoin) {
-                        let resDataAdd = transactionRepository.postAddAmount(user, amount, appId);
+                    } else if (parseInt(method) === historyTransaction.methods.addCoin) {
+                        let resDataAdd = await transactionRepository.postAddAmount(user, amount, appId);
                         if (resDataAdd.status === 200) {
                             await historyTransaction.findByIdAndUpdate({ _id: transactionId}, { $set: { status: 1 }});
                         }
